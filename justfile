a2d3382bd0873753fb5ac52e802a4d48d96f8c6a
luxluth <delphin.blehoussi93@gmail.com>
2026-02-24 16:12:52 +0100

    setting up the monorepo correctly

--- justfile
+++
@@ -1,8 +0,0 @@
-build: build-core build-react
-
-build-react:
-  cd packages/anode-react && pnpm build
-
-build-core:
-  pnpm build
-
--- packages/anode-react/tsdown.config.ts
+++ packages/anode-react/tsdown.config.ts
@@ -4,6 +4,5 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   platform: 'browser',
   dts: true,
-  unbundle: true,
-  noExternal: ['anode']
+  unbundle: true
 });
